ac6991c85fdc8ee0f9933716e4f5a957a590aea5
Zhengjun Li <zhengjun@asu.edu>
2021-07-24 23:13:49 -0500

    162 nlogn binary search implementation

--- Easy/162-find-peak-element.py
+++ Easy/162-find-peak-element.py
@@ -14,3 +14,29 @@ class Solution:
                     return i
             return len(nums) - 1
          
+'''
+O(NlogN)
+Uses binary search to find the peak. At index mid, peak will either be left, mid, or right.
+So we can use binary search to find it.
+'''
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        high = len(nums) - 1
+        low = 0  
+        while high != low:
+            mid = int((high - low) / 2 + low)
+            print(f'{low}, {mid}, {high}')
+            if nums[mid] < nums[mid+1]:
+                # peak on right
+                low = mid + 1
+            elif mid == 0:
+                #edge case handler
+                return 0
+            elif nums[mid-1] > nums[mid]:
+                # peak on left
+                high = mid - 1
+            else:
+                # at peak
+                return mid
+        return low #when low == high, it is peak, simply return
+        
